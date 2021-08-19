cf33f7215a8812b462190f425788bb2623c56adb
hao <hao.quan@icloud.com>
2021-08-19 09:13:13 +0200

    Pass robotic dataset for MSG3D model. But the summary writer is WIP.

--- model/msg3d.py
+++ model/msg3d.py
@@ -110,7 +110,7 @@ class Model(nn.Module):
                  num_gcn_scales,
                  num_g3d_scales,
                  graph,
-                 in_channels=3):
+                 in_channels=2):
         super(Model, self).__init__()
 
         Graph = import_class(graph)
@@ -124,9 +124,11 @@ class Model(nn.Module):
         c3 = c2 * 2     # 384
 
         # r=3 STGC blocks
-        self.gcn3d1 = MultiWindow_MS_G3D(3, c1, A_binary, num_g3d_scales, window_stride=1)
+        # self.gcn3d1 = MultiWindow_MS_G3D(3, c1, A_binary, num_g3d_scales, window_stride=1)
+        self.gcn3d1 = MultiWindow_MS_G3D(2, c1, A_binary, num_g3d_scales, window_stride=1)
         self.sgcn1 = nn.Sequential(
-            MS_GCN(num_gcn_scales, 3, c1, A_binary, disentangled_agg=True),
+            # MS_GCN(num_gcn_scales, 3, c1, A_binary, disentangled_agg=True),
+            MS_GCN(num_gcn_scales, 2, c1, A_binary, disentangled_agg=True),
             MS_TCN(c1, c1),
             MS_TCN(c1, c1))
         self.sgcn1[-1].act = nn.Identity()
